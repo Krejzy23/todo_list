e883b85986d71243f53c1261ca0424df969eeb7b
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-26 08:01:05 +0200

    add UI/UIX icons and theme,Sidebar funcionality

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,13 +1,36 @@
-"use client"
-import React from 'react'
-import { styled } from 'styled-components'
+"use client";
+import React from "react";
+import { styled } from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
 
 const Sidebar = () => {
+  const { theme } = useGlobalState();
 
   return (
-    <SidebarStyled>Sidebar</SidebarStyled>
-  )
-}
-const SidebarStyled = styled.nav``;
+    <SidebarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay"></div>
+        <div className="image">
+          <Image width={70} height={70} src="/avatar1.webp" alt="profile" />
+        </div>
+        <h1>
+          <span className="">Sakura</span>
+          <span className="">Haruno</span>
+        </h1>
+      </div>
+      <ul className="nav-items">
+        
+      </ul>
+    </SidebarStyled>
+  );
+};
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
 
-export default Sidebar
+export default Sidebar;
--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -1,17 +1,29 @@
-"use client"
-import React from 'react'
-import GlobalStyleProvider from './GlobalStyleProvider';
+"use client";
+import React from "react";
+import { GlobalProvider } from "../context/globalProvider";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 function ContextProvider({ children }: Props) {
-  return (
-    <GlobalStyleProvider>
-      {children}
-    </GlobalStyleProvider>
-  )
+  const [isReady, setIsReady] = React.useState(false)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 250);
+  }, []);
+
+  if (!isReady) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <span className="loader"></span>
+      </div>
+    );
+  }
+
+  return <GlobalProvider>{children}</GlobalProvider>;
 }
 
-export default ContextProvider
+export default ContextProvider;
